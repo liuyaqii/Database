e189c0019f4a965860c55b460404d8135d2caab2
liuyaqii <115548611+liuyaqii@users.noreply.github.com>
2022-10-21 20:53:08 +0800

    Delete q2_northamerican.sql

--- q2_northamerican.sql
+++
@@ -1,9 +0,0 @@
-select Id,ShipCountry,'Northamerican' as place
-from "Order"
-where Id>=15445 and ShipCountry in ('USA','Mexico','Canada')
-UNION
-select Id,ShipCountry,'Otherplace' as place
-from "Order"
-where Id>=15445 and ShipCountry not in ('USA','Mexico','Canada')
-order by Id;
-
