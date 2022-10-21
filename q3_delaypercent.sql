538f7f4e974fd9d35a680fdb32bb52d2a1ed97d2
liuyaqii <115548611+liuyaqii@users.noreply.github.com>
2022-10-21 20:53:19 +0800

    Delete q3_delaypercent.sql

--- q3_delaypercent.sql
+++
@@ -1,6 +0,0 @@
-
-select CompanyName,round(round(sum(CASE WHEN (ShippedDate>"Order".RequiredDate)THEN 1 ELSE 0 END ),4)/count(*)*100,2) as percentage
-from "Order",Shipper
-where "Order".ShipVia=Shipper.Id
-group by CompanyName
-order by percentage;
