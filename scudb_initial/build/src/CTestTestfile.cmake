ada7cdff56de7425256479776212ace75e89e729
liuyaqii <2424925106@qq.com>
2022-11-21 07:56:04 -0800

    zuoye2

---
+++ scudb_initial/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sya7/Document/Database/scudb_initial/src
+# Build directory: /home/sya7/Document/Database/scudb_initial/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
